df9f0e025ba86496ab6f93ec6c1719a2f9fb1a32
Muhammad Awais <m.awais2030@gmail.com>
2024-05-01 03:48:20 +0500

    Created Logo Component with Lucide React

---
+++ components/Logo.tsx
@@ -0,0 +1,16 @@
+import { DollarSign } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+function Logo() {
+  return (
+    <Link href='/' className='flex items-center gap-2'>
+      <DollarSign className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
+      <p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
+        BudgetTracker
+      </p>
+    </Link>
+  )
+}
+
+export default Logo
